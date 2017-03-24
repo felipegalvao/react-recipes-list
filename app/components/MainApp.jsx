98163b15262fc817138c0db14bad96698966643f
Felipe Galvao <contato@felipegalvao.com.br>
2017-03-24 18:02:41 -0300

    Basic project setup with Webpack, React, Sass and Babel

---
+++ app/components/MainApp.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class MainApp extends Component {
+    render() {
+        return (
+            <div>
+                <p>Main App Component</p>
+            </div>
+        );
+    }
+}
+
+export default MainApp;
