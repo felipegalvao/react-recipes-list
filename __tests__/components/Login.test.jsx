541f6919ddfdab93f0d5cd0f235c155bfe16f274
Felipe Galvao <contato@felipegalvao.com.br>
2017-03-29 01:09:08 -0300

    Add tests to the Login component

---
+++ __tests__/components/Login.test.jsx
@@ -0,0 +1,45 @@
+import React from 'react';
+import chai from 'chai';
+import sinon from 'sinon';
+import { shallow, mount } from 'enzyme';
+import Login from '../../app/components/Login.jsx';
+
+var expect = chai.expect;
+
+describe("Login", () => {
+    it('should render an image, a h1 title and a form', () => {        
+        var login = shallow(<Login/>);
+        expect(login.find('img').length).to.equal(1);
+        expect(login.find('h1').length).to.equal(1);
+        expect(login.find('form').length).to.equal(1);
+    })
+
+    it('should call onLogin prop when complete form is submitted', () => {
+        const spy = sinon.spy();
+        var login = shallow(<Login onLogin={spy}/>);
+        login.find('#email').first().simulate('change', {target: {value: 'felipe@galvao.com'} });
+        login.find('#password').first().simulate('change', {target: {value: 'abcdef'} });
+        login.find('form').simulate('submit', { preventDefault(){} });
+
+        expect(spy.calledOnce).to.be.true;
+    })
+
+    it('should not call onLogin prop when incomplete form is submitted', () => {
+        const spy = sinon.spy();
+        var login = shallow(<Login onLogin={spy}/>);
+        login.find('#email').first().simulate('change', {target: {value: 'felipe@galvao.com'} });
+        login.find('form').simulate('submit', { preventDefault() {} });
+
+        expect(spy.calledOnce).to.be.false;
+    })
+
+    it('should not call onLogin prop when invalid email is submitted', () => {
+        const spy = sinon.spy();
+        var login = shallow(<Login onLogin={spy}/>);
+        login.find('#email').first().simulate('change', {target: {value: 'felipe'} });
+        login.find('#password').first().simulate('change', {target: {value: 'abcdef'} });
+        login.find('form').simulate('submit', { preventDefault() {} });
+
+        expect(spy.calledOnce).to.be.false;
+    })
+})
--- app/components/Login.jsx
+++ app/components/Login.jsx
@@ -1,25 +1,47 @@
 import React, { Component } from 'react';
 
 class Login extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
     handleLogin = (event) => {
         event.preventDefault();
-        this.props.onLogin();
+        if (this.state.email !== '' && this.state.password !== '') {
+            var re = /\S+@\S+\.\S+/;
+            if (re.test(this.state.email) === true) {
+                this.props.onLogin();
+            }            
+        }        
+    }
+
+    handleEmailChange = (event) => {
+        this.setState({email: event.target.value});
+    }
+
+    handlePasswordChange = (event) => {
+        this.setState({password: event.target.value});
     }
 
     render() {
         return (
             <div className="login-component">
-                <img src={require("../images/hellofresh-logo.svg")} />
+                <img src="https://www.hellofresh.com/images/hellofresh-logo.svg?v=3" />
                 <h1>HelloFresh Recipes List</h1>
 
                 <form onSubmit={this.handleLogin}>
                     <div className="form-group">
                         <label htmlFor="email">Email</label>
-                        <input type="email" id="email" name="email" placeholder="Insert your email" required />
+                        <input type="email" id="email" ref="email" name="email" placeholder="Insert your email" onChange={this.handleEmailChange} required />
                     </div>
                     <div className="form-group">
                         <label htmlFor="password">Password</label>
-                        <input type="password" id="password" name="password" placeholder="Insert your password" required />
+                        <input type="password" id="password" ref="password" name="password" placeholder="Insert your password" onChange={this.handlePasswordChange} required />
                     </div>
                     <div className="form-group">
                         <button>Log In</button>
